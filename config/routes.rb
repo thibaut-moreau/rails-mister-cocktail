2005734bf41c405529574150f836d2f8fe0f729e
Thibaut Moreau <thibaut.moreau.massart@gmail.com>
2020-02-21 16:46:04 +0100

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktail#index"
+
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
